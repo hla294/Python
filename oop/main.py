7b33c2c75f124906ded0fb0dbf950a73bac4fd66
Samuel Lee <hla294@sfu.ca>
2022-11-24 00:41:03 -0800

    added an attributed to the table

--- oop/main.py
+++ oop/main.py
@@ -15,4 +15,6 @@ table = PrettyTable()
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
 table.add_column("Type", ["Electric", "Water", "Fire"])
 
+table.align = 'l' #prettytable's attribute
+
 print(table)
