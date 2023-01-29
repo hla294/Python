850c9a145b0a2cd324fc6b5cbe993e1a68bf3018
Samuel Lee <hla294@sfu.ca>
2023-01-29 09:10:13 -0800

    highest score implemented

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -9,6 +9,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0  # score 0 initializing
+        self.high_score = 0
         self.color("white")  # color of the score board
         self.penup()  # avoiding drawing a line to the position of the score
         self.goto(0, 270)  # score board location
@@ -16,13 +17,19 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)  # writing a score board
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)  # writing a score board
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write(f"GAME OVER", align = ALIGNMENT, font = FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write(f"GAME OVER", align = ALIGNMENT, font = FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear() # delete the previous score to avoid a new score being written over the previous score
         self.update_scoreboard()
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -39,6 +39,13 @@ class Snake:
         # add a new segmen to the snake getting longer.
         self.add_segment(self.segments[-1].position())
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def move(self):
         """
         Movign snake
