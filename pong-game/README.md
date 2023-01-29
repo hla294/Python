48acd0966a098e077d9443c7648425696fb00032
Samuel Lee <hla294@sfu.ca>
2023-01-28 22:19:42 -0800

    implemented the paddle

--- pong-game/README.md
+++ pong-game/README.md
@@ -1,6 +1,12 @@
 # Development Plans for Pong Game !
 ## 1. Create the screen
 ## 2. Create and move a paddle
+### - paddle should be able to move 'up' and 'down' by up and down keyboard buttons
+### - attributes of the paddle:
+#### width = 20
+#### height = 100
+#### x position = 350
+#### y position = 0
 ## 3. Create another paddle
 ## 4. Create the ball and make it move
 ## 5. Detect collision with wall and bounce
--- pong-game/main.py
+++ pong-game/main.py
@@ -1,8 +1,31 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong Game by Samuel Lee")
+screen.tracer(0) # automatic screen updates are off, no animation in the beginning of the screen
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350,0)
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)  # only updates the position of y (+20)
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)  # only updates the position of y (-20)
+
+screen.listen()
+screen.onkey(go_up, "Up")  # onkey(function, key)
+screen.onkey(go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
