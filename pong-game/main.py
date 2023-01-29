91b6c8cf23d7a661ff978e940316f6fd9d9cf880
Samuel Lee <hla294@sfu.ca>
2023-01-28 21:52:37 -0800

    implemented the screen

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,1 +1,8 @@
-from turtle import Turtle
+from turtle import Screen
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong Game by Samuel Lee")
+
+screen.exitonclick()
