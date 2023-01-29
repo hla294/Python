6514591023820b41574458185f20da62a22cb587
Samuel Lee <hla294@sfu.ca>
2023-01-29 09:46:19 -0800

    storing highest score in data.txt

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -9,7 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0  # score 0 initializing
-        self.high_score = 0
+        with open("data.txt") as data:  # loading the highest score from the previous game
+            self.high_score = int(data.read())
         self.color("white")  # color of the score board
         self.penup()  # avoiding drawing a line to the position of the score
         self.goto(0, 270)  # score board location
@@ -23,6 +24,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:  # updating the highest score in the data.txt file
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
