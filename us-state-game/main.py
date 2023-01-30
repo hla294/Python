7e2780dd09f06f4667acd3f994a14c0ddbfde6d1
Samuel Lee <hla294@sfu.ca>
2023-01-29 21:14:39 -0800

    implementation using pandas and turtle

--- us-state-game/main.py
+++ us-state-game/main.py
@@ -1,13 +1,31 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S.A States Game !")
-image = "blank"
-
+image = "blank_states_img.gif"
 screen.addshape(image)
+turtle.shape(image)
 
-turtle.shape
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What is another state's name?").title()
+    print(answer_state)
 
+    # If answer_state is one of the states in all the states of the 50_states.csv
+    if answer_state in all_states:
+        #if they got it right:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+            #Create a turtle to write the name of the state at the state's corx and cory
 
 screen.exitonclick()
