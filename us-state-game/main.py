996bc5bec1a17b69fbb80f3ec7526da1d52975e9
Samuel Lee <hla294@sfu.ca>
2023-01-29 21:21:11 -0800

    states to learn implementation

--- us-state-game/main.py
+++ us-state-game/main.py
@@ -16,9 +16,16 @@ while len(guessed_states) < 50:
                                     prompt="What is another state's name?").title()
     print(answer_state)
 
-    # If answer_state is one of the states in all the states of the 50_states.csv
+    if answer_state == "Exit":  # Ending the game
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
     if answer_state in all_states:
-        #if they got it right:
+
         guessed_states.append(answer_state)
         t = turtle.Turtle()
         t.hideturtle()
@@ -26,6 +33,6 @@ while len(guessed_states) < 50:
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(state_data.state.item())
-            #Create a turtle to write the name of the state at the state's corx and cory
 
-screen.exitonclick()
+# States_to_learn.csv
+
