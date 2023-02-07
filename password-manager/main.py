ed02f3ef61202f804a9f56f75f54b244c7e9bb57
Samuel Lee <hla294@sfu.ca>
2023-02-06 21:21:23 -0800

    basic canvas with a picture implement

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,5 +1,17 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Welcome to Password Manager!")  # adding title to the window
+window.config(padx=20, pady =20)  # adding 20 padding to x and y
+
+canvas = Canvas(height=200, width=200)  # creating height & width size 200 canvas
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img) # Expecting some tuple to work on. create_image(position, **options)
+canvas.pack() # canvas layout dimension
+
+window.mainloop() # tells Python to run the Tkinter event loop. Running until you close the window.
