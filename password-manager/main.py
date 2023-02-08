a0163f79162dbdaefc6a93379466f427444bd0c4
Samuel Lee <hla294@sfu.ca>
2023-02-07 20:56:37 -0800

    search password by website implemented

--- password-manager/main.py
+++ password-manager/main.py
@@ -59,6 +59,23 @@ def save():
         finally:
             website_entry.delete(0, END)  # Deleting the website entry after adding
             password_entry.delete(0, END)  # Deleting the password entry after adding
+# ---------------------------- FIND PASSWORD------------------------------- #
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data file Found!!")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -80,8 +97,8 @@ password_label = Label(text="Password: ")
 password_label.grid(row=3, column=0)  # shouldn't put "width" -> not working !
 
 # Entries
-website_entry = Entry(width=36)
-website_entry.grid(row=1, column=1, columnspan = 2)  # Extending column 1 to column 2
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()  # User can directly type here as soon as the program runs
 email_entry = Entry(width=36)
 email_entry.grid(row=2, column=1, columnspan =2)
@@ -94,5 +111,7 @@ generate_password_button = Button(text="Generate Password", command = generate_p
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+search_button = Button(text="Search", width=13, command = find_password)
+search_button.grid(row=1, column=2, columnspan=3)
 
 window.mainloop() # tells Python to run the Tkinter event loop. Running until you close the window.
