327040f9ec771e0ed74e75844060c7b0080469ec
Samuel Lee <hla294@sfu.ca>
2023-02-07 20:22:45 -0800

    "FileNotFoundError" handling implemented

--- password-manager/main.py
+++ password-manager/main.py
@@ -42,16 +42,21 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you enter all the fields!!")
     else:
-
-        with open("data.json", "r") as data_file:
-            # Reading old data
-            data = json.load(data_file)
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
             # Updating old data with new data
             data.update(new_data)
-        with open("data.json", "w") as data_file:
-            # Saving updated data
-            json.dump(data, data_file, indent=4)  # Write json file
 
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)  # Write json file
+        finally:
             website_entry.delete(0, END)  # Deleting the website entry after adding
             password_entry.delete(0, END)  # Deleting the password entry after adding
 
