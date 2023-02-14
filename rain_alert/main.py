835d9698e7a185b4271c967c6f1b90feda285994
Samuel Lee <hla294@sfu.ca>
2023-02-13 21:48:51 -0800

    weather API implementation

---
+++ rain_alert/main.py
@@ -0,0 +1,30 @@
+import requests
+
+OWM_Endpoint = "https://api.openweathermap.org/data/3.0/onecall "
+api_key = "70ad3656455080593dd1fd0a7eafbfec"
+
+weather_params = {
+    "lat": 49.282730,
+    "lon": -123.120735,
+    "appid": api_key,
+    "exclude": "current, minutely, daily",
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella.")
+
+
+# print(weather_data["hourly"][0]["weather"][0]["id"])
+
